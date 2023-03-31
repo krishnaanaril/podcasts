17bfbe26f9bbb8c5f6b0cc66004a81f388112d8a
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-03-31 10:38:40 +0530

    feat: add pwa support

--- projects/podcasts-ui/src/app/app.module.ts
+++ projects/podcasts-ui/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule,    
-    HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
