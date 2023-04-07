d7bc24f4c3ce17c9dd933fdce36e346ec604db26
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-04-07 15:05:21 +0530

    fix: search navigation

--- projects/podcasts-ui/src/app/components/search/search.component.ts
+++ projects/podcasts-ui/src/app/components/search/search.component.ts
@@ -4,6 +4,7 @@ import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
 import { DataService } from '../../services/data.service';
 import { SearchFeed } from '../../models/shared.type';
 import { InfoCardComponent } from '../info-card/info-card.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'pc-search',
@@ -16,24 +17,48 @@ export class SearchComponent implements OnInit {
 
   searchSub$ = new Subject<string>();
   searchFeeds: Array<SearchFeed> = [];
+  userInput = '';
 
-  constructor(private location: Location, private dataService: DataService){
+  constructor(private location: Location, private route: ActivatedRoute, private router: Router, private dataService: DataService){
 
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
+    this.route.queryParams.subscribe(params => {
+      const searchText = params['text'];
+      if(searchText) {        
+        const decodedSearchText = decodeURIComponent(searchText);
+        this.userInput = decodedSearchText;
+        this.searchByText(decodedSearchText);
+      }
+    });    
+
     this.searchSub$.pipe(
       debounceTime(400),
       distinctUntilChanged()
     ).subscribe((searchText: string) => {
       const normalizedSearchText = searchText.trim().toLowerCase();
-      this.dataService.searchByTerm(normalizedSearchText).subscribe({
-        next: result => this.searchFeeds = result.feeds,
-        error: error => console.error(error),
-        complete: () => console.info('searchByTerm complete')
+      const encodedSearchText = encodeURIComponent(normalizedSearchText);
+      console.log(this.userInput);
+      const queryParams = {
+        text: encodedSearchText,        
+      };
+      const urlTree = this.router.createUrlTree([], {
+        queryParams: queryParams,
+        relativeTo: this.route,
       });
+      this.location.replaceState(urlTree.toString());      
+      this.searchByText(normalizedSearchText);
     });
   }  
+
+  searchByText(text: string) {
+    this.dataService.searchByTerm(text).subscribe({
+      next: result => this.searchFeeds = result.feeds,
+      error: error => console.error(error),
+      complete: () => console.info('searchByTerm complete')
+    });
+  }
 
   backButtonClick() {
     this.location.back();
