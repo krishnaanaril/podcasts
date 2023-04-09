6958873de2872a6ab96afc06d621cd2e6fab784a
krishnaanaril <krishnamohan.a.m@gmail.com>
2023-04-09 07:40:24 +0530

    minor: add episode details placeholder

---
+++ projects/podcasts-ui/src/app/components/episode-details/episode-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'pc-episode-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './episode-details.component.html',
+  styleUrls: ['./episode-details.component.css']
+})
+export class EpisodeDetailsComponent {
+
+}
